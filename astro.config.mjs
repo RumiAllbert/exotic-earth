cf89b607a9f32a0fab01d701e2e4bc612f6fe420
Rumi Allbert <rumiallbert@gmail.com>
2024-09-05 16:37:26 -0400

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
-import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), icon()]
 });
